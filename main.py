6807269a34200cec8d6aa0d135337ca996f8595d
Abdullahi Mohamud <abdullahimohamud.9917@gmail.com>
2025-12-01 15:11:23 +0300

    save my first python program

---
+++ main.py
@@ -0,0 +1,18 @@
+#writing my first 'Hello World'
+
+print("hello world")
+
+#Asking user their name.
+
+name =input('what is your name? ')
+print("your name is: " + name)
+
+#Asking user their age.
+
+age =input('what is your age? ')
+print = ("you are: "+ age + "years old")
+ 
+#Generating an error for learning purpose.
+
+musi= "hello python crash course reader "
+print(musi)    
