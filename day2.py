4ef916ec1b10de53033e8ab675f300e331ac8264
Abdullahi Mohamud <abdullahimohamud.9917@gmail.com>
2025-12-02 15:29:19 +0300

    commit day2.py

---
+++ day2.py
@@ -0,0 +1,42 @@
+#Ask user for their name.
+#greetings= input('what is your name? ') 
+#print(f'hello, {greetings}')
+
+#practising print keywordsgreetings
+#print("hello","world", end="!!!", sep="-") 
+#print("Hello,", '"friends"')
+#print("hello,\"friends\"") -> use escaping to print string enclosed within double qoutes.
+
+#removing extra white spaces.
+#ame=input('whats your name? ')
+#me=name.strip()
+#capitalizing string
+#ame=name.title()
+#'whats your age? ')
+#rint(f'hell dear, {name}')
+
+
+#.Spliting variables.
+
+# name=input('what is your name? ')
+# name=name.split()
+# print(f'your name is: {name}')
+
+#. Getting first name and last name
+#.splitting user's name 
+# user_name=input('whats your name?')
+# first, last = user_name.split()
+
+
+#. Ask user for an input.
+# 
+
+#. create these string variables.
+
+string1="hello world"
+string2="it's CS50P"
+string3="C:\new_folder/"
+string4="""line1 line2"""
+print(string1+ '-' +string2+ '-' + string3 + '.')
+print(string1+ string2 + '-'+ string3)
+print(string4)
